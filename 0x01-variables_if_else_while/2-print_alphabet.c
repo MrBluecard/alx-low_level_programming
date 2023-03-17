acf23550a217eb7a8f304ed1f90410c4cf594054
MrBluecard <youssefelyacouti6@gmail.com>
2023-03-17 13:59:21 -0700

    celseif

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char p;
+
+	for (p = 'a' ; p <= 'z' ; p++)
+	{
+		putchar(p);
+	}
+	putchar('\n');
+	return (0);
+}
+
