68d18a718df78db0386a821639ff975b47355c35
MrBluecard <youssefelyacouti6@gmail.com>
2023-03-21 20:14:29 -0700

    cfunnesloop

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @g: integer input
+ * Return: absolute value of g
+ */
+int _abs(int g)
+	{
+		if (g >= 0)
+		{
+			return (g);
+		}
+		return (-g);
+	}
+
